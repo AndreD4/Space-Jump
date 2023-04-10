ef0f003c805738a978d63023f267d290a25712f9
AndreD4 <andremai629@gmail.com>
2023-04-10 09:37:39 +0900

    Input.GetAxis for horizontal and vertical Axis

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerControl : MonoBehaviour
 {
+    [SerializeField] float xThrowSpeed = 10f;
+    [SerializeField] float yThrowSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,15 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       float xThrow = Input.GetAxis("Horizontal");
+       float yThrow = Input.GetAxis("Vertical");
+      
+      float xOffset = xThrow * Time.deltaTime * xThrowSpeed;
+      float newXPos = transform.localPosition.x + xOffset;
+
+      float yOffset = yThrow * Time.deltaTime * yThrowSpeed;
+      float newYPos = transform.localPosition.y + yOffset;
         
+      transform.localPosition = new Vector3 (newXPos,newYPos,transform.localPosition.z);
     }
 }
